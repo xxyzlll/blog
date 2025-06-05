e8981e845bd27cda2900f3159a48b284571eac1f
yangzhilin <2582325957@qq.com>
2025-06-05 17:28:32 +0800

    feat：字数

--- src/contants/article.ts
+++ src/contants/article.ts
@@ -6,7 +6,7 @@ export const baseList: Article[] = [
         description: '惰性执行（Lazy Execution）是一种编程技术，它可以在需要时才计算结果，从而提高程序的性能。在 vue3 中，惰性执行主要应用于响应式系统和计算属性',
         publishDate: '2024-11-22',
         mdPath: '/markdown/Vue 3 的惰性执行原理.md',
-        wordCount: 1500,
+        wordCount: 836,
         title: 'vue3 的惰性执行原理',
         id: '',
         imageUrl: ''
@@ -15,7 +15,7 @@ export const baseList: Article[] = [
         description: '我们的公共服务体系是非常友好的，在市场监督管理单位注册个体工商户，是免费的。就连打印营业执照，也不收工本费。理论上，我们自己拿着身份证，去城市里的行政服务中心柜台办理，就可以。但是，对于我们普通人来说，不熟悉流程，很多材料准备不齐全，可能需要跑好几趟。',
         publishDate: '2024-11-22',
         mdPath: '/markdown/手把手教学：个体工商户营业执照“0元购”.md',
-        wordCount: 1500,
+        wordCount: 3524,
         title: '手把手教学：个体工商户营业执照“0元购”',
         id: '',
         imageUrl: ''
